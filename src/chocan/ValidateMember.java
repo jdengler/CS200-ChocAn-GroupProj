463ad1cbb422089c6743fb11b51a22dee26056a0
jdengler <jdengler@crimson.ua.edu>
2018-11-26 16:49:25 -0600

    Added ValidateMember

---
+++ src/chocan/ValidateMember.java
@@ -0,0 +1,20 @@
+package chocan;
+
+public class ValidateMember {
+	
+	
+	private CardSwipe swipeCard() {
+		CardSwipe swipe = new CardSwipe();
+		
+		return swipe;
+	}
+	
+	private void enterNumber(int memberNumber) {
+		CardSwipe swipe = new CardSwipe();
+		
+	}
+	
+	public static void main() {
+		
+	}
+}
