f0ab4c36d6889b8b24db4a7ec84eb1cdc089183e
jdengler <jdengler@crimson.ua.edu>
2018-11-25 20:14:17 -0600

    Added ProviderDirectory and ProviderDirectoryEntry files

---
+++ src/chocan/ProviderDirectoryEntry.java
@@ -0,0 +1,12 @@
+package chocan;
+
+public class ProviderDirectoryEntry {
+	
+	private String serviceName;
+	private int serviceCode;
+	private double serviceFee;
+	
+	public ProviderDirectoryEntry() {
+		
+	}
+}
