fc56a58f219a10465a003e88a5394a4378d94ad3
caspear1 <caspear1@crimson.ua.edu>
2018-11-28 14:18:44 -0600

    Operator Read member

--- src/chocan/OperatorTerminal.java
+++ src/chocan/OperatorTerminal.java
@@ -1,9 +1,36 @@
 package chocan;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class OperatorTerminal extends Terminal {
 	
 	public void Menu() {
 		System.out.println("Menu\n1. Manage Accounts\n2. Manage Provider\n");
+	}
+	
+	public void readMembers() throws FileNotFoundException{
+		Scanner buffer = new Scanner(new File("Members.txt"));
+	    
+	    Scanner input = new Scanner(buffer.toString()).useDelimiter(",|\n");
+	
+	    ManageAccounts ma = new ManageAccounts();
+	    
+	    while(input.hasNext()) {
+	        String name = input.next();
+	        int number = input.nextInt();
+	        String address = input.next();
+	        String city = input.next();
+	        String state = input.next();
+	        int zipCode = input.nextInt();
+	        boolean status = input.nextBoolean();
+	       
+	        ma.addMember(name, number, address, city, state, zipCode, status);
+	    }
+	    
+	    buffer.close();
+	    input.close();
 	}
 
 }
--- src/chocan/ProviderTerminal.java
+++ src/chocan/ProviderTerminal.java
@@ -6,29 +6,6 @@ import java.util.Scanner;
 
 public class ProviderTerminal extends Terminal {
 	
-	public void readMembers() throws FileNotFoundException{
-		Scanner buffer = new Scanner(new File("Members.txt"));
-	    
-	    Scanner input = new Scanner(buffer.toString()).useDelimiter(",|\n");
-	
-	    ManageAccounts ma = new ManageAccounts();
-	    
-	    while(input.hasNext()) {
-	        String name = input.next();
-	        int number = input.nextInt();
-	        String address = input.next();
-	        String city = input.next();
-	        String state = input.next();
-	        int zipCode = input.nextInt();
-	        boolean status = input.nextBoolean();
-	        
-	
-	        
-	        ma.addMember(name, number, address, city, state, zipCode, status);
-	    }
-	    
-	    buffer.close();
-	    input.close();
-	}
+
 
 }
