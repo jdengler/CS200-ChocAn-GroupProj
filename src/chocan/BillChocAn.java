56381db72d8b0d2b0ab372433e3d48dcd7f99834
ahphan <ahphan@crimson.ua.edu>
2018-11-27 20:49:56 -0600

    Created Bill ChocAn

---
+++ src/chocan/BillChocAn.java
@@ -0,0 +1,48 @@
+package chocan;
+import java.util.*;
+
+public class BillChocAn {
+
+    private ServiceRecord billService = new ServiceRecord();
+    
+    public BillChocAn() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public void setCurrentDate() {
+        Date current = new Date(); //Get current date and time
+        
+        billService.setCurrentDateTime(current);
+    }
+    
+    public void setDateOfService() {
+        Scanner reader = new Scanner(System.in); 
+        System.out.print("Enter Month: ");
+        int month = reader.nextInt();
+        System.out.print("Enter Day: ");
+        int day = reader.nextInt();
+        System.out.print("Enter Year: ");
+        int year = reader.nextInt();
+        reader.close();
+        
+        @SuppressWarnings("deprecation")
+        Date date = new Date(year,month - 1, day);
+        
+        billService.setDateOfService(date);
+    }
+    
+    public void enterComments() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter comments: ");
+        String comments = reader.nextLine();
+        billService.setComments(comments);
+        reader.close();
+    }
+    
+    public void enterServiceCode() {
+        billService.setServiceCode();
+        
+        
+    }
+
+}
