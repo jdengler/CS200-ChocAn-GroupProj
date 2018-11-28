99b71c8cdec68eb86bdd811178536b4099cf199f
Namito Yokota <nyokota@crimson.ua.edu>
2018-11-28 14:42:54 -0600

    managerMenu() added

--- src/chocan/ManagerTerminal.java
+++ src/chocan/ManagerTerminal.java
@@ -1,7 +1,44 @@
 package chocan;
+import java.util.*;
 
 public class ManagerTerminal extends Terminal{
 	
-	
+	public void managerMenu() {
+		
+		Scanner reader = new Scanner(System.in);
+		int option = 0;
+		
+		while(true) {
+			System.out.println("\tManager Terminal");
+			System.out.println("1.) Generate Summary Report"); 
+			System.out.println("2.) Generate Member Report"); 
+			System.out.println("3.) Generate Provider Report"); 
+			System.out.println("4.) Quit");
+			System.out.println("\tOption: ");
+			
+			option = reader.nextInt();
+			
+			if (option == 1) {
+				// generate summary report
+				continue;
+			}
+			
+			else if (option == 2) {
+				// generate member report
+				continue;
+			}
+			
+			else if (option == 3) {
+				// generate provider report
+				continue;
+			}
+			
+			else {
+				break;
+			}
+		}
+		
+		reader.close();
+	}
 
 }
