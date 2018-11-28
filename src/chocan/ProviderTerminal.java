0d73104519941ac03f5a5646d4bb7b154f550fd9
hesposi2278 <hesposito@crimson.ua.edu>
2018-11-28 15:13:57 -0600

    Added partial constructor to validateMember and coded providerTerminal

--- src/chocan/ProviderTerminal.java
+++ src/chocan/ProviderTerminal.java
@@ -6,13 +6,41 @@ import java.util.Scanner;
 
 public class ProviderTerminal extends Terminal {
   
-  
+  private static Scanner sc = new Scanner(System.in);
   
   public static void providerMenu() {
     System.out.println("--------Provider Terminal--------\n\n" +
                        "Press (1) to validate a ChocAn member\n" +
                        "Press (2) to bill ChocAn\n" +
                        "Press (3) to request a provider directory");
+    int userChoice = sc.nextInt();
+    switch(userChoice) {
+        case 1:
+          validateMember();
+          break;
+        case 2:
+          billChocan();
+          break;
+        case 3:
+          requestDirectory();
+          break;
+        default:
+          System.out.println("Please enter a valid choice...");
+          break;
+    }
+  }
+  
+  private static void validateMember() {
+    System.out.print("Please enter member number: ");
+    int memNum = sc.nextInt();
+    ValidateMember x = new ValidateMember(memNum);
+  }
+  
+  private static void billChocan() {
+    
+  }
+  
+  private static void requestDirectory() {
     
   }
 
