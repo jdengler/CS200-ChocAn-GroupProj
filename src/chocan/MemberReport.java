91fb1ce847f681ac7c157629bb09195f723ba787
hesposi2278 <hesposito@crimson.ua.edu>
2018-11-27 21:19:25 -0600

    Edited member report

--- src/chocan/MemberReport.java
+++ src/chocan/MemberReport.java
@@ -1,8 +1,22 @@
 package chocan;
 import java.util.Date;
+import java.util.ArrayList;
 
 public class MemberReport {
 
+	private ArrayList<ServiceRecord> servicesProvided;
 	
+	public MemberReport(ArrayList<ServiceRecord> servicesProvided) {
+	  this.servicesProvided = servicesProvided;
+	}
 	
+	public ArrayList<ServiceRecord> getServicesProvided() {
+	  return this.servicesProvided;
+	}
+	
+	public void setServicesProvided(ArrayList<ServiceRecord> servicesProvided) {
+	  this.servicesProvided = servicesProvided;
+	}
+	
+	public void 
 }
