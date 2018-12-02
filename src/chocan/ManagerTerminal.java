808015ee5de4acef406e4e8728289bcd92259970
ahphan <ahphan@crimson.ua.edu>
2018-12-02 15:27:07 -0600

    Moving functions from terminal to ManagerTerminal (as a private)

--- src/chocan/ManagerTerminal.java
+++ src/chocan/ManagerTerminal.java
@@ -1,5 +1,8 @@
 package chocan;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
@@ -65,4 +68,104 @@ public class ManagerTerminal extends Terminal{
         
         return;
     }
+	
+	/*
+     * Prints the summary report (used by Manager)
+     */
+	private void printSummaryReport() throws FileNotFoundException,IOException{
+	      ArrayList<ProviderAccounts> providers = ReadProviders();
+	      Scanner reader = new Scanner(System.in);
+	      System.out.println("Please name the file: ");
+	      String name = reader.nextLine();
+	      
+	      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ServiceRecords/"+name+".txt";
+	      double totalFee = 0;
+	      int totalConsults = 0;
+	      
+	      File file = new File(filename);
+	      /*Path oPath = Paths.get(filename);
+	      File oFile = oPath.toFile();*/
+	      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
+	          for (ProviderAccounts pa : providers) {
+	              ArrayList<ServiceRecord> sr = pa.getServicesProvided();
+	              input.write(pa.getName()+","+pa.getNumberServices()+","+pa.getTotalFees(sr)+"\n");
+	              totalConsults = totalConsults + pa.getNumberServices();
+	              totalFee = totalFee + pa.getTotalFees(sr);
+	          }
+	          input.write("Total number of providers: "+providers.size()+"\n");
+	          input.write("Total number of consultations: "+totalConsults+"\n");
+	          input.write("Total number of fees: $"+totalFee+"\n");
+	      input.close();
+	      }
+	      
+	      System.out.print("\n"+"The summary report was successfully generated and stored in "+name+".txt"+"\n");
+	      reader.close();
+	}
+	
+	/*
+     * Prints the member report (used by Manager)
+     */
+	  public void printMemberReport(MemberAccounts member) throws FileNotFoundException, IOException{
+	      Scanner reader = new Scanner(System.in);
+	      System.out.println("Please name the file: ");
+	      String name = reader.nextLine();
+	      
+	      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/MemberReports/"+name+".txt";
+	      
+	      
+	      File file = new File(filename);
+	      /*Path oPath = Paths.get(filename);
+	      File oFile = oPath.toFile();*/
+	      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
+	          input.write(member.getName()+"\n"+member.getNumber()+"\n"+member.getAddress()+"\n"+member.getCity()+"\n"+member.getState()+"\n"+member.getZipCode()+"\n");
+	          
+	          ArrayList<ServiceRecord> sr = member.getServicesProvided();
+	          if (sr != null) {
+	              for(ServiceRecord service : sr) {
+	                  input.write(service.getDateOfService()+"\n"+service.getProviderName(service.getProviderNumber())+"\n"+service.getServiceName(service.getServiceCode())+"\n");
+	              }  
+	          }
+
+	          input.close();
+	      }
+	      
+	      System.out.print("\n"+"The member report was successfully generated and stored in "+name+".txt"+"\n");
+	      reader.close();
+	  }
+	  
+	
+	  /*
+       * Prints the provider report (used by Manager)
+       */
+	  public void printProviderReport(ProviderAccounts provider) throws FileNotFoundException, IOException{
+	      //ArrayList<ProviderAccounts> pa = ReadProviders();
+	      
+	      Scanner reader = new Scanner(System.in);
+	      System.out.println("Please name the file: ");
+	      String name = reader.nextLine();
+	      double totalFee = 0;
+	      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ProviderReports/"+name+".txt";
+	      
+	      
+	      File file = new File(filename);
+	      /*Path oPath = Paths.get(filename);
+	      File oFile = oPath.toFile();*/
+	      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
+	          input.write(provider.getName()+"\n"+provider.getNumber()+"\n"+provider.getAddress()+"\n"+provider.getCity()+"\n"+provider.getState()+"\n"+provider.getZipCode()+"\n");
+	          
+	          ArrayList<ServiceRecord> sr = provider.getServicesProvided();
+	          if (sr != null) {
+	              for(ServiceRecord service : sr) {
+	                  input.write(service.getDateOfService()+"\n"+service.getCurrentDateTime()+"\n"+service.getMemberName(service.getMemberNumber())+"\n"+service.getMemberNumber()+"\n"+service.getServiceCode()+"\n"+service.getServiceFee(service.getServiceCode())+"\n");
+	                  totalFee = totalFee + service.getServiceFee(service.getServiceCode());
+	              }  
+	          }
+	          input.write("Total number of consultations with members: "+sr.size()+"\n");
+	          input.write("Total fee for the week: $"+totalFee+"\n");
+	          input.close();
+	      }
+	      
+	      System.out.print("\n"+"The provider report was successfully generated and stored in "+name+".txt"+"\n");
+	      reader.close();
+	  }
 }
--- src/chocan/Terminal.java
+++ src/chocan/Terminal.java
@@ -69,7 +69,11 @@ public class Terminal {
 	  OperatorTerminal ot = new OperatorTerminal();
 	  ot.operaterMenu();
   }
-
+  
+  
+/*
+ * Reads in the providerDirectory
+ */
   public ArrayList<ProviderDirectoryEntry> ReadProviderDirectoryEntry() throws FileNotFoundException, IOException {
       String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ProviderReports/providerDirectory.txt";
       Path pPath = Paths.get(filename);
@@ -100,6 +104,9 @@ public class Terminal {
       return listPde;
   }
   
+  /*
+   * Reads in members and the services they've had
+   */
   public ArrayList<MemberAccounts> ReadMember() throws FileNotFoundException, IOException{
       String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/MemberReports/member.txt";
       Path mPath = Paths.get(filename);
@@ -153,6 +160,9 @@ public class Terminal {
       return listMemA;
   }
   
+  /*
+   * Reads in the providers and the services they've provided
+   */
   public ArrayList<ProviderAccounts> ReadProviders() throws FileNotFoundException, IOException{
       String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ProviderReports/provider.txt";
       Path mPath = Paths.get(filename);
@@ -210,6 +220,9 @@ public class Terminal {
       return listProvA;
   }
   
+  /*
+   * Rewrites the member.txt (Used for operator purposes such as add/delete/update member)
+   */
   public void printDatabase(ArrayList<MemberAccounts> ma) throws FileNotFoundException, IOException{
       Scanner reader = new Scanner(System.in);
       System.out.println("Please name the file: ");
@@ -239,7 +252,9 @@ public class Terminal {
   }
   
 
-  
+  /*
+   * Prints the providers (Used for operator purposes such as add/delete/update provider)
+   */
   public void printProviders(ArrayList<ProviderAccounts> pa) throws FileNotFoundException, IOException{
       String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ProviderReports/provider.txt";
       Path oPath = Paths.get(filename);
@@ -260,96 +275,13 @@ public class Terminal {
       }
   }
   
-  public void printMemberReport(MemberAccounts member) throws FileNotFoundException, IOException{
-      Scanner reader = new Scanner(System.in);
-      System.out.println("Please name the file: ");
-      String name = reader.nextLine();
-      
-      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/MemberReports/"+name+".txt";
-      
-      
-      File file = new File(filename);
-      /*Path oPath = Paths.get(filename);
-      File oFile = oPath.toFile();*/
-      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
-          input.write(member.getName()+"\n"+member.getNumber()+"\n"+member.getAddress()+"\n"+member.getCity()+"\n"+member.getState()+"\n"+member.getZipCode()+"\n");
-          
-          ArrayList<ServiceRecord> sr = member.getServicesProvided();
-          if (sr != null) {
-              for(ServiceRecord service : sr) {
-                  input.write(service.getDateOfService()+"\n"+service.getProviderName(service.getProviderNumber())+"\n"+service.getServiceName(service.getServiceCode())+"\n");
-              }  
-          }
-
-          input.close();
-      }
-      
-      System.out.print("\n"+"The member report was successfully generated and stored in "+name+".txt"+"\n");
-      reader.close();
-  }
   
-  public void printProviderReport(ProviderAccounts provider) throws FileNotFoundException, IOException{
-      //ArrayList<ProviderAccounts> pa = ReadProviders();
-      
-      Scanner reader = new Scanner(System.in);
-      System.out.println("Please name the file: ");
-      String name = reader.nextLine();
-      double totalFee = 0;
-      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ProviderReports/"+name+".txt";
-      
-      
-      File file = new File(filename);
-      /*Path oPath = Paths.get(filename);
-      File oFile = oPath.toFile();*/
-      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
-          input.write(provider.getName()+"\n"+provider.getNumber()+"\n"+provider.getAddress()+"\n"+provider.getCity()+"\n"+provider.getState()+"\n"+provider.getZipCode()+"\n");
-          
-          ArrayList<ServiceRecord> sr = provider.getServicesProvided();
-          if (sr != null) {
-              for(ServiceRecord service : sr) {
-                  input.write(service.getDateOfService()+"\n"+service.getCurrentDateTime()+"\n"+service.getMemberName(service.getMemberNumber())+"\n"+service.getMemberNumber()+"\n"+service.getServiceCode()+"\n"+service.getServiceFee(service.getServiceCode())+"\n");
-                  totalFee = totalFee + service.getServiceFee(service.getServiceCode());
-              }  
-          }
-          input.write("Total number of consultations with members: "+sr.size()+"\n");
-          input.write("Total fee for the week: $"+totalFee+"\n");
-          input.close();
-      }
-      
-      System.out.print("\n"+"The provider report was successfully generated and stored in "+name+".txt"+"\n");
-      reader.close();
-  }
   
-  public void printSummaryReport() throws FileNotFoundException,IOException{
-      ArrayList<ProviderAccounts> providers = ReadProviders();
-      Scanner reader = new Scanner(System.in);
-      System.out.println("Please name the file: ");
-      String name = reader.nextLine();
-      
-      String filename = "/Users/ashleyphan/git/cs200fall2018team2/src/chocan/writtenFiles/ServiceRecords/"+name+".txt";
-      double totalFee = 0;
-      int totalConsults = 0;
-      
-      File file = new File(filename);
-      /*Path oPath = Paths.get(filename);
-      File oFile = oPath.toFile();*/
-      try(BufferedWriter input = new BufferedWriter(new FileWriter(file))){
-          for (ProviderAccounts pa : providers) {
-              ArrayList<ServiceRecord> sr = pa.getServicesProvided();
-              input.write(pa.getName()+","+pa.getNumberServices()+","+pa.getTotalFees(sr)+"\n");
-              totalConsults = totalConsults + pa.getNumberServices();
-              totalFee = totalFee + pa.getTotalFees(sr);
-          }
-          input.write("Total number of providers: "+providers.size()+"\n");
-          input.write("Total number of consultations: "+totalConsults+"\n");
-          input.write("Total number of fees: $"+totalFee+"\n");
-      input.close();
-      }
-      
-      System.out.print("\n"+"The summary report was successfully generated and stored in "+name+".txt"+"\n");
-      reader.close();
-  }
   
+  
+  /*
+   * Prints the providerDirectory
+   */
   public void rewriteDirectory() throws FileNotFoundException, IOException{
       ArrayList<ProviderDirectoryEntry> pde = ReadProviderDirectoryEntry();
 
