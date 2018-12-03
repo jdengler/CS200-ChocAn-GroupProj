52b241ee385dcebb381914cd4ee507e4cc62b25d
Hunter Esposito <hesposito@crimson.ua.edu>
2018-12-03 16:35:44 -0600

    Updated GenerateReport Test

--- src/chocan/GenerateReportTest.java
+++ src/chocan/GenerateReportTest.java
@@ -2,9 +2,18 @@ package chocan;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
 public class GenerateReportTest {
 
-  @org.junit.Test
-  public void generateMember() {
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testGenerateMember() throws FileNotFoundException, IOException {
   }
 }
