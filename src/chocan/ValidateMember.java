e0bace62826656da3e5ba08c746821607bdb7ee8
Hunter Esposito <hesposito@crimson.ua.edu>
2018-12-05 11:15:00 -0600

    Added javadoc info to ValidateMember

--- src/chocan/ValidateMember.java
+++ src/chocan/ValidateMember.java
@@ -1,5 +1,9 @@
-/*
- * Author: Hunter Espocito
+/**
+ * ValidateMember: Used to find a user defined member number in the system and check to see whether
+ * it is a valid number and checks if the member is suspended or not.
+ *
+ * @author Hunter Esposito
+ * @version 3.5
  */
 
 package chocan;
@@ -12,31 +16,41 @@ import javax.swing.JOptionPane;
 
 
 public class ValidateMember {
-	
-	public static MemberAccounts validateMember(int memNum) throws FileNotFoundException, IOException{
-	    ArrayList<MemberAccounts> me = Terminal.ReadMember();
-	    int found = -1;
-	    MemberAccounts ma = new MemberAccounts();
-	    ma.setStatus(false);
-	    
-	    for(MemberAccounts account : me) {
-	        if(account.getNumber() == memNum) {
-	            found = 0;
-	            if (account.getStatus() == false) {
-	                //System.out.println("\nSuspended");
-	                JOptionPane.showMessageDialog(null, "The member with member number "+memNum+" has been suspended.");
-	            }
-	            else {
-	                //System.out.println("\nValidated");
-	                JOptionPane.showMessageDialog(null, "Valid member number");
-	            }
-	            ma = account;
-	        }
-	    }
-	    if (found == -1) {
-	        //System.out.println("\nInvalid");
-	        JOptionPane.showMessageDialog(null, "Invalid member number");
-	    }
-	    return ma;
-	  }
+
+  /**
+   * The validate member function, validates user inputted member number
+   *
+   * @param memNum The member number to search for
+   * @return If corresponding member number is found, return that MemberAccounts obj,
+   *         else, return empty MemberAccounts obj.
+   * @throws FileNotFoundException Throws if members.txt is unable to be found.
+   * @throws IOException Throws if there are problems with file IO
+   */
+  public static MemberAccounts validateMember(int memNum)
+      throws FileNotFoundException, IOException {
+    ArrayList<MemberAccounts> me = Terminal.ReadMember();
+    int found = -1;
+    MemberAccounts ma = new MemberAccounts();
+    ma.setStatus(false);
+
+    for (MemberAccounts account : me) {
+      if (account.getNumber() == memNum) {
+        found = 0;
+        if (account.getStatus() == false) {
+          //System.out.println("\nSuspended");
+          JOptionPane.showMessageDialog(null,
+              "The member with member number " + memNum + " has been suspended.");
+        } else {
+          //System.out.println("\nValidated");
+          JOptionPane.showMessageDialog(null, "Valid member number");
+        }
+        ma = account;
+      }
+    }
+    if (found == -1) {
+      //System.out.println("\nInvalid");
+      JOptionPane.showMessageDialog(null, "Invalid member number");
+    }
+    return ma;
+  }
 }
